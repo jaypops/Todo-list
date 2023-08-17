bd18c93b0ce712ed74dcd12df32717a43e997d27
Timothy-TO <73559338+jaypops@users.noreply.github.com>
2023-08-17 20:03:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,39 +6,41 @@ const deletee = document.querySelector(".delete");
 const container = document.querySelector(".container");
 const itemList = JSON.parse(localStorage.getItem("todos")) || [];
 const cancelClass = "cancel";
+const creatNewList = function (inputValues) {
+  const html = `
+    <li class="list-of-items">
+      <div class="items" id="hidden">
+        <p class="items">${inputValues}</p>
+      </div>
+      <div class="DelCal">
+        <span>
+          <i class="fa fa-solid fa-trash ${cancelClass} item"></i>
+        </span>
+      </div>
+    </li>
+  `;
+  textCopy.insertAdjacentHTML("afterbegin", html);
+};
 plus.addEventListener("submit", function (e) {
   e.preventDefault();
   if (TextFeild.value === "") {
     alert("Input a task");
   } else {
     const inputValue = TextFeild.value.trim();
-    const creatNewList = function () {
-      const html = `
-        <li class="list-of-items">
-          <div class="items" id="hidden">
-            <p>${inputValue}</p>
-          </div>
-          <div class="DelCal">
-            <span>
-              <i class="fa fa-solid fa-trash ${cancelClass} item"></i>
-            </span>
-          </div>
-        </li>
-      `;
-      textCopy.insertAdjacentHTML("afterbegin", html);
-    };
-    creatNewList();
+    creatNewList(inputValue);
     TextFeild.value = "";
-    textCopy.addEventListener("click", function (e) {
-      e.preventDefault();
-      const clicked = e.target.closest(".item");
-      if (!clicked) return;
-      if (!clicked.classList.contains("cancelClass")) {
-        e.target.parentElement.parentElement.parentElement.remove();
-      }
-    });
     itemList.push(inputValue);
     localStorage.setItem("todos", JSON.stringify(itemList));
-    console.log(localStorage.getItem("todos"));
   }
+});
+textCopy.addEventListener("click", function (e) {
+  e.preventDefault();
+  const clicked = e.target.closest(".item");
+  if (!clicked) return;
+  if (!clicked.classList.contains("cancelClass")) {
+    e.target.parentElement.parentElement.parentElement.remove();
+  }
+});
+window.addEventListener("load", function () {
+  itemList.forEach((item) => creatNewList(item));
 });
